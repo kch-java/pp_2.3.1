e11c36bd7ea064c276f07327b242ab2f441ef233
kch <kch@rambler.ru>
2023-05-10 03:44:48 +0300

    solution_1

---
+++ src/main/java/web/controller/CarsController.java
@@ -0,0 +1,31 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.service.CarService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/cars")
+public class CarsController {
+
+    private final CarService carService;
+
+    @Autowired
+    public CarsController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping()
+    public String getCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
+        List<Car> cars = (count >= 5) ? carService.getAllCars() : carService.listCars(count);
+        model.addAttribute("cars", cars);
+        return "cars";
+    }
+}
